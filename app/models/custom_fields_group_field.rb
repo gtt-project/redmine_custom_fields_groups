aad6fe6d33e91c865e887093a503d74933cf5c44
Ko Nagase <nagase@georepublic.co.jp>
2021-10-14 08:42:20 +0900

    Added custom field column on views/custom_fields_groups/index
    Added CustomFieldsGroupField sorted scope

--- app/models/custom_fields_group_field.rb
+++ app/models/custom_fields_group_field.rb
@@ -2,5 +2,7 @@ class CustomFieldsGroupField < ActiveRecord::Base
   belongs_to :custom_fields_group
   belongs_to :custom_field
 
-  # scope :sorted, lambda { order(:position) }
+  scope :sorted, (lambda do
+    includes(:custom_field).order("#{CustomField.table_name}.position ASC")
+  end)
 end

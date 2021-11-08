20c8b6a4c12918d7fd060249a5043daf9f18a715
Ko Nagase <nagase@georepublic.co.jp>
2021-11-08 12:57:19 +0900

    Fixed deface app/overrides relative path

--- lib/redmine_custom_fields_groups.rb
+++ lib/redmine_custom_fields_groups.rb
@@ -1,5 +1,5 @@
 Rails.application.paths["app/overrides"] ||= []
-Rails.application.paths["app/overrides"] << File.expand_path("../app/overrides", __FILE__)
+Rails.application.paths["app/overrides"] << File.expand_path("../../app/overrides", __FILE__)
 
 Rails.configuration.to_prepare do
   require 'redmine_custom_fields_groups/hooks/view_layouts_base_html_head_hook'

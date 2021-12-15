ea302f2e31ccb6e64b2ed187d54ce55a66f9b70a
Ko Nagase <nagase@georepublic.co.jp>
2021-12-15 14:21:14 +0900

    Fixed Options translation in production env

--- lib/redmine_custom_fields_groups/options.rb
+++ lib/redmine_custom_fields_groups/options.rb
@@ -1,14 +1,18 @@
 module RedmineCustomFieldsGroups
   class Options
     include Redmine::I18n
-    GROUP_TAGS = [
-      [l(:label_group_tag_h3), "h3"],
-      [l(:label_group_tag_h4), "h4"],
-      [l(:label_group_tag_fieldset), "fieldset"]
-    ]
-    FIELDSET_STATES = [
-      [l(:label_fieldset_state_all_expended), "all_expended"],
-      [l(:label_fieldset_state_all_collapsed), "all_collapsed"],
-    ]
+    def self.group_tags
+      [
+        [l(:label_group_tag_h3), "h3"],
+        [l(:label_group_tag_h4), "h4"],
+        [l(:label_group_tag_fieldset), "fieldset"]
+      ]
+    end
+    def self.fieldset_states
+      [
+        [l(:label_fieldset_state_all_expended), "all_expended"],
+        [l(:label_fieldset_state_all_collapsed), "all_collapsed"],
+      ]
+    end
   end
 end

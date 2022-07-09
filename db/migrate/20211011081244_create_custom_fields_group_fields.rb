f25f4838831f98c8ed2e58e2cca373c2410525c2
Ko Nagase <nagase@georepublic.co.jp>
2022-07-10 01:12:19 +0900

    Fixed migrate error on mysql

--- db/migrate/20211011081244_create_custom_fields_group_fields.rb
+++ db/migrate/20211011081244_create_custom_fields_group_fields.rb
@@ -2,7 +2,7 @@ class CreateCustomFieldsGroupFields < ActiveRecord::Migration[5.2]
   def change
     create_table :custom_fields_group_fields, id: false do |t|
       t.references :custom_fields_group, index: true, foreign_key: true
-      t.references :custom_field, index: { unique: true }, foreign_key: true
+      t.references :custom_field, index: { unique: true }, foreign_key: true, type: :integer
 
       # t.timestamps null: false
     end

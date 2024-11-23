a0687ef6ae068725648289c22f96eb337844589f
Ko Nagase <nagase@georepublic.co.jp>
2024-11-24 00:25:38 +0900

    Fix sqlite3 adapter fixtures :custom_fields_group_fields delete issue temporarily

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,14 @@
 # Load the Redmine helper
 require_relative '../../../test/test_helper'
 
-ActiveRecord::FixtureSet.create_fixtures(
-  File.dirname(__FILE__) + '/fixtures',
-  ['custom_fields_groups', 'custom_fields_group_fields']
-)
+def load_plugin_fixtures(reset_cache = false)
+  if reset_cache
+    ActiveRecord::FixtureSet.reset_cache
+  end
+  ActiveRecord::FixtureSet.create_fixtures(
+    File.dirname(__FILE__) + '/fixtures',
+    ['custom_fields_groups', 'custom_fields_group_fields']
+  )
+end
+
+load_plugin_fixtures

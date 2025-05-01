97461f01066257a9d024d8e2a163a8344c5a4a98
Ko Nagase <nagase@georepublic.co.jp>
2025-05-01 17:55:23 +0900

    Drop Redmine <= 5.0 and adjust CI

--- init.rb
+++ init.rb
@@ -7,9 +7,9 @@ Redmine::Plugin.register :redmine_custom_fields_groups do
   author_url 'https://github.com/georepublic'
   url 'https://github.com/gtt-project/redmine_custom_fields_groups'
   description 'This is a plugin for grouping custom fields'
-  version '2.0.0'
+  version '2.1.0'
 
-  requires_redmine :version_or_higher => '5.0.0'
+  requires_redmine :version_or_higher => '5.1.0'
 
   settings partial: 'settings/redmine_custom_fields_groups',
     default: {
